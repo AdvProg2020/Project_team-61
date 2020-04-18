67936ac3679113c9cb927332d3e176f07fec0aef
742-hub <azarnooshsa@gmail.com>
2020-04-18 19:37:31 +0430

    SellerMenu CustomerMenu SaleMenu

--- src/main/java/controller/menus/CustomerMenu.java
+++ src/main/java/controller/menus/CustomerMenu.java
@@ -1,8 +1,63 @@
 package controller.menus;
 
 
+import view.OutputHandler;
+
 public class CustomerMenu {
+    private int inputNo;
+    private int outputNo;
 
+    public void processViewCart() {
+        // OutputHandler.showObjectOutput(account.listUsers(), account.getAccountListSize(),1);
 
+    }
+
+    public void showProducts() {
+
+    }
+
+    public void viewProduct(String productID) {
+
+    }
+
+    public void increaseProductNumber(String productID) {
+
+    }
+
+    public void decreaseProductNumber(String productID) {
+
+    }
+
+    public void showTotalPrice() {
+
+    }
+
+    public void purchase() {
+
+    }
+
+    public void processPurchase() {
+
+    }
+
+    public void processViewOrders() {
+
+    }
+
+    public void showOrder(String orderID) {
+
+    }
+
+    public void rateProduct(String productID, int number) {
+
+    }
+
+    public void processViewBalance() {
+
+    }
+
+    public void processViewDiscountCodes() {
+
+    }
 
 }
