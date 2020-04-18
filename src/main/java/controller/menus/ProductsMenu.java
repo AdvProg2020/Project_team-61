0554f8ae917efe6ee6a911efc904eb0bcfb0f64a
742-hub <azarnooshsa@gmail.com>
2020-04-18 19:40:11 +0430

    ProductMenu ProductsMenu

--- src/main/java/controller/menus/ProductsMenu.java
+++ src/main/java/controller/menus/ProductsMenu.java
@@ -1,5 +1,65 @@
 package controller.menus;
 
 public class ProductsMenu {
+    private int outputNo;
+    private int inputNo;
 
+    public void processProducts() {
+
+    }
+
+    public void processViewCategories() {
+
+    }
+
+    public void processFiltering() {
+
+    }
+
+    public void showAvailableFilters() {
+
+    }
+
+    public void filter(String filterID) {
+
+    }
+
+    public void currentFilters() {
+
+    }
+
+    public void disableFilter(String filterID) {
+
+    }
+
+
+    public void processSorting() {
+
+    }
+
+    public void showAvailableSorts() {
+
+    }
+
+    public void sort(String sortID) {
+
+    }
+
+
+    public void currentSorts() {
+
+    }
+
+    public void disableSort() {
+
+    }
+
+
+    public void processShowProducts() {
+
+    }
+
+    public void processShowProductsID(String id) {
+
+    }
 }
