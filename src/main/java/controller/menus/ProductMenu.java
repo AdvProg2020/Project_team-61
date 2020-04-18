020c54e96c7b751b62005087cd04864c5b5ec3e0
742-hub <azarnooshsa@gmail.com>
2020-04-18 19:40:11 +0430

    ProductMenu ProductsMenu

--- src/main/java/controller/menus/ProductMenu.java
+++ src/main/java/controller/menus/ProductMenu.java
@@ -1,4 +1,39 @@
 package controller.menus;
+
+
+import model.productRelated.Comment;
+
 public class ProductMenu {
+    private int outputNo;
+    private int inputNo;
+    private Comment comment;
+
+    public void processDigest() {
+
+    }
+
+    public void addToCart(){
+
+    }
+
+    public void selectSeller(String username){
+
+    }
+
+    public void processAttributes() {
+
+    }
+
+    public void processCompare(String productID) {
+
+    }
+
+    public void processComments() {
+
+    }
+
+    public void addComments(){
+
+    }
 
 }
