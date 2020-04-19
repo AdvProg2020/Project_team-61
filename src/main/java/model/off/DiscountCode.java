cada0345df89e132e7f5fb716b50605673fab94f
heia <helia>
2020-04-19 01:25:19 -0700

    sale and discount and category methods(without compare and view)

--- src/main/java/model/off/DiscountCode.java
+++ src/main/java/model/off/DiscountCode.java
@@ -1,4 +1,85 @@
-package model.off;
+package main.java.model.off;
+import java.util.*;
+import java.util.ArrayList;
 
 public class DiscountCode {
+    private int discountId;
+    private Date startOfDiscountPeriod;
+    private Date endOfDiscountPeriod;
+    private double maxDiscountAmount;
+    private int totalTimesOfUse;
+    private ArrayList<model.accounts.Account> allCustomersWithDiscountCode;
+    private static ArrayList<DiscountCode> allDiscountCodes;
+
+    public DiscountCode(int discountId, Date startOfDiscountPeriod, Date endOfDiscountPeriod, double maxDiscountAmount, int totalTimesOfUse, ArrayList<model.accounts.Account> allCustomersWithDiscountCode) {
+        this.discountId = discountId;
+        this.startOfDiscountPeriod = startOfDiscountPeriod;
+        this.endOfDiscountPeriod = endOfDiscountPeriod;
+        this.maxDiscountAmount = maxDiscountAmount;
+        this.totalTimesOfUse = totalTimesOfUse;
+        this.allCustomersWithDiscountCode =new ArrayList <>();
+        allDiscountCodes.add(this);
+    }
+
+    public void setStartOfDiscountPeriod(Date startOfDiscountPeriod) {
+        this.startOfDiscountPeriod = startOfDiscountPeriod;
+    }
+
+    public void setEndOfDiscountPeriod(Date endOfDiscountPeriod) {
+        this.endOfDiscountPeriod = endOfDiscountPeriod;
+    }
+
+    public void setMaxDiscountAmount(double maxDiscountAmount) {
+        this.maxDiscountAmount = maxDiscountAmount;
+    }
+
+    public int getDiscountId() {
+        return discountId;
+    }
+
+    public Date getStartOfDiscountPeriod() {
+        return startOfDiscountPeriod;
+    }
+
+    public Date getEndOfDiscountPeriod() {
+        return endOfDiscountPeriod;
+    }
+
+    public double getMaxDiscountAmount() {
+        return maxDiscountAmount;
+    }
+
+    public int getTotalTimesOfUse() {
+        return totalTimesOfUse;
+    }
+
+    public ArrayList<model.accounts.Account> getAllCustomersWithDiscountCode() {
+        return allCustomersWithDiscountCode;
+    }
+
+    public boolean isThereDiscountWithId(int id){
+        for (DiscountCode discountCode : allDiscountCodes) {
+            if(discountCode.getDiscountId() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public DiscountCode getDiscountWithId(int id){
+        for ( DiscountCode discountcode:allDiscountCodes){
+            if (discountcode.getDiscountId()==id){
+                return discountcode;
+            }
+        }
+        return null;
+    }
+    public void deleteDiscount(int id){
+        allDiscountCodes.remove(getDiscountWithId(id));
+
+    }
+ /*   public void viewDiscountCodes(){
+        for (DiscountCode discountCode:allDiscountCodes){
+            di
+        }
+    }*/
 }
--- src/main/java/model/off/Sale.java
+++ src/main/java/model/off/Sale.java
@@ -1,4 +1,123 @@
-package model.off;
-
+package main.java.model.off;
+import java.util.ArrayList;
+import java.util.*;
 public class Sale {
+    private String offId;
+    private model.off.SaleStatus saleStatus;
+    private Date startOfSalePeriod;
+    private Date endOfSalePeriod;
+    private int saleAmount;
+    private model.accounts.Seller seller;
+    private ArrayList <model.productRelated.Product>allProducts;
+    private static ArrayList <Sale> allSales;
+
+    public Sale(String offId, model.off.SaleStatus saleStatus, Date startOfSalePeriod, Date endOfSalePeriod, int saleAmount, model.accounts.Seller seller) {
+        this.offId = offId;
+        this.saleStatus = saleStatus;
+        this.startOfSalePeriod = startOfSalePeriod;
+        this.endOfSalePeriod = endOfSalePeriod;
+        this.saleAmount = saleAmount;
+        this.seller = seller;
+        allProducts=new ArrayList<>();
+        allSales.add(this);
+
+    }
+
+    public String getOffId() {
+        return offId;
+    }
+
+    public model.off.SaleStatus getSaleStatus() {
+        return saleStatus;
+    }
+
+    public Date getStartOfSalePeriod() {
+        return startOfSalePeriod;
+    }
+
+    public Date getEndOfSalePeriod() {
+        return endOfSalePeriod;
+    }
+
+    public int getSaleAmount() {
+        return saleAmount;
+    }
+
+    public model.accounts.Seller getSeller() {
+        return seller;
+    }
+
+    public ArrayList<model.productRelated.Product> getAllProducts() {
+        return allProducts;
+    }
+
+    public  Sale getSaleWithId(String id){
+        for (main.java.model.off.Sale sale : allSales) {
+            if (sale.getOffId().equals(id)) {
+                return sale;
+            }
+        }
+        return null;
+    }
+    public boolean isThereSaleWithId(String id){
+       return allSales.contains(getSaleWithId(id));
+    }
+
+
+     public int getAllSalesSize(){
+            return allSales.size();
+    }
+    public Sale getSaleWithSeller(model.accounts.Seller seller){
+        for (Sale sale:allSales){
+            if ((sale.getSeller())==(seller)){
+                return sale;
+            }
+
+        }
+        return null;
+    }
+     public Sale getSaleWithProduct(model.productRelated.Product product){
+         for (Sale sale : allSales) {
+             for (model.productRelated.Product products : sale.getAllProducts()) {
+                 if (products.equals(product)){
+                     return sale;
+             }
+         }
+         }
+         return null;
+     }
+
+    public void setStartOfSalePeriod(Date startOfSalePeriod) {
+        this.startOfSalePeriod = startOfSalePeriod;
+    }
+
+    public void setEndOfSalePeriod(Date endOfSalePeriod) {
+        this.endOfSalePeriod = endOfSalePeriod;
+    }
+
+    public void setSaleAmount(int saleAmount) {
+        this.saleAmount = saleAmount;
+    }
+
+    public boolean isTheProductInAnotherSale(model.productRelated.Product product) {
+        for (Sale sale : allSales) {
+            if (sale.getAllProducts().contains(product)) {
+                return true;}
+        }
+        return false;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Sale{" +
+                "offId='" + offId + '\'' +
+                ", saleStatus=" + saleStatus +
+                ", startOfSalePeriod=" + startOfSalePeriod +
+                ", endOfSalePeriod=" + endOfSalePeriod +
+                ", saleAmount=" + saleAmount +
+                ", seller=" + seller +
+                ", allProducts=" + allProducts +
+                '}';
+    }
 }
