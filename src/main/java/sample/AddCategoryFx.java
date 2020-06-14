101cbf9431b2adc4bdd4d90ad19f0d91da6e047c
haxtap <heliarosha@yahoo.com>
2020-06-14 16:18:13 +0430

    sale cat discount

---
+++ src/main/java/sample/AddCategoryFx.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import model.off.DiscountCode;
+
+public class AddCategoryFx {
+
+
+
+    @FXML
+    public ImageView Exit;
+    @FXML
+    public ImageView Back;
+    @FXML
+    public ImageView Account;
+
+    public void createCategory(MouseEvent mouseEvent) {
+    }
+
+    public void gotoAccount(MouseEvent mouseEvent) {
+    }
+
+    public void processExit(MouseEvent mouseEvent) {
+    }
+
+    public void processBack(MouseEvent mouseEvent) {
+    }
+
+
+
+}
+
+
---
+++ src/main/java/sample/AddDiscountFx.java
@@ -0,0 +1,131 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import model.off.DiscountCode;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class AddDiscountFx {
+
+
+
+    private DiscountCode newDiscountCode;
+    @FXML
+    public ImageView Exit;
+    @FXML
+    public ImageView Back;
+    @FXML
+    public ImageView Account;
+    @FXML
+    public TextField discountIdTextField;
+    @FXML
+    public Spinner timesOfUseSpinner;
+    @FXML
+    public Spinner discountAmountSpinner;
+    @FXML
+    public DatePicker EndOfDiscountDatePicker;
+    @FXML
+    public DatePicker StartOfDiscountDatePicker;
+    @FXML
+    public Label discountIdAlertLabel;
+    @FXML public Label StartDiscountAlertLabel;
+    @FXML public Label EndDiscountAlertLabel;
+    public void setDiscountIdTextField(){
+        String input=discountIdTextField.getText();
+        //newDiscountCode.
+    }
+    public void setDiscountAmount(){
+        SpinnerValueFactory<Integer> valueFactory=new SpinnerValueFactory.IntegerSpinnerValueFactory(5,100,1,5);
+        discountAmountSpinner=new Spinner();
+        discountAmountSpinner.setValueFactory(valueFactory);
+        discountAmountSpinner.setEditable(true);
+        newDiscountCode.setDiscountAmount(Integer.parseInt(discountAmountSpinner.getValue().toString()));
+
+    }
+    //?annonation lazeme?
+    @FXML
+    public void setTimesOfUseSpinner(){
+        SpinnerValueFactory<Integer> valueFactory=new SpinnerValueFactory.IntegerSpinnerValueFactory(1,30,2,1) ;
+        timesOfUseSpinner=new Spinner();
+        timesOfUseSpinner.setValueFactory(valueFactory);
+        timesOfUseSpinner.setEditable(true);
+        newDiscountCode.setTotalTimesOfUse(Integer.parseInt(timesOfUseSpinner.getValue().toString()));
+
+    }
+    @FXML
+    public void setStartOfDiscountDate(){
+        if (discountIdTextField.getText()==null){
+            StartOfDiscountDatePicker.setDisable(true);
+        }
+        else {
+            LocalDate inputLocalDate;
+            inputLocalDate =StartOfDiscountDatePicker.getValue();
+            Date inputDate;
+            Date now=new Date();
+            ZoneId defaultZoneId = ZoneId.systemDefault();
+            inputDate = Date.from(inputLocalDate.atStartOfDay(defaultZoneId).toInstant());
+            if (now.after(inputDate)){
+                StartDiscountAlertLabel.setText("Your date must be after today!");
+            }else
+                newDiscountCode.setStartOfDiscountPeriod(inputDate);
+        }
+
+    }
+    public void setEndOfDiscountDate(){
+        if (StartOfDiscountDatePicker.getValue()==null){
+            EndOfDiscountDatePicker.setDisable(true);
+        }
+        else {
+            LocalDate inputLocalDate;
+            inputLocalDate =StartOfDiscountDatePicker.getValue();
+            Date inputDate;
+            Date now=new Date();
+            ZoneId defaultZoneId = ZoneId.systemDefault();
+            inputDate = Date.from(inputLocalDate.atStartOfDay(defaultZoneId).toInstant());
+            if (inputDate.before(now)||inputDate.before(newDiscountCode.getStartOfDiscountPeriod())){
+                EndDiscountAlertLabel.setText("End of Discount Date period is invalid!");
+            }
+            else
+                newDiscountCode.setEndOfDiscountPeriod(inputDate);
+        }
+
+    }
+
+    public void processBack(MouseEvent mouseEvent) {
+    }
+
+    public void processExit(MouseEvent mouseEvent) {
+    }
+
+    public void gotoAccount(MouseEvent mouseEvent) {
+    }
+
+   /* @FXML
+
+    @FXML
+    @FXML
+    @FXML
+    @FXML
+    @FXML
+    @FXML
+    @FXML*/
+
+
+}
+
